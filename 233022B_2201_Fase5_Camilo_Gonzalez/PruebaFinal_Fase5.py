f31a1da50f04015660bcbec47b6bf3b4031dac0c
camilodesarrollouniversidad-cmyk <camilodesarrollouniversidad@gmail.com>
2026-05-22 21:51:10 -0500

    codigo con comentarios

---
+++ 233022B_2201_Fase5_Camilo_Gonzalez/PruebaFinal_Fase5.py
@@ -0,0 +1,26 @@
+# funcion para calcular la cantidad a pedir
+def cantidad_a_pedir(stock_actual, stock_minimo):
+  
+    # si el stock actual es menor al mínimo, se debe pedir la diferencia entre ambos, de lo contrario no se debe pedir nada (0 unidades)
+    if stock_actual < stock_minimo:
+        return stock_minimo - stock_actual
+    return 0
+
+# lista de artículos con su código, nombre, stock actual y stock mínimo
+
+articulos = [
+    [101, "Lapicero", 12, 20],
+    [102, "Cuaderno", 25, 15],
+    [103, "Resaltador", 5, 10],
+    [104, "Carpeta", 6, 8],
+    [105, "Papel bond", 3, 12],
+]
+
+# programa principal
+if __name__ == "__main__":
+    print("Lista de pedidos:")
+   # se recorre la lista de artículos y se calcula la cantidad a pedir para cada uno, mostrando solo aquellos que necesitan ser pedidos 
+    for codigo, nombre, stock_actual, stock_minimo in articulos:
+        pedir = cantidad_a_pedir(stock_actual, stock_minimo)
+        if pedir > 0:
+            print(f"- {nombre}: {pedir} unidad(es)")
